42c070deec659617a5d5606f7f40c063fc83c557
dankato <apearmor@gmail.com>
2017-12-27 23:43:44 -0800

    nodemailer added

--- Node/express-website/app.js
+++ Node/express-website/app.js
@@ -21,5 +21,36 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
+
+app.post("/contact/send", function(req, res) {
+  var transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "email@gmail.com",
+      pass: "pass"
+    }
+    // https://myaccount.google.com/lesssecureapps
+  });
+  var mailOptions = {
+    from: "FirstName LastName <email@gmail.com>",
+    to: "email@example.com",
+    subject: "Website Form Submission",
+    text: "You have a new submission!",
+    html: "<p>This is a paragraph</p>"
+  };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.redirect("/");
+    } else {
+      console.log("message sent");
+      res.redirect("/");
+    }
+  });
+});
+
 app.listen(3000);
 console.log("server is running on port 3000");
